3d63ac6f677c4a40f913ce584c8ac2e6f7a91db9
Lydia.Fritz <Lydia.Fritz@STEMADJ-35448.gcu.edu>
2021-11-09 06:44:06 -0700

    added 4th class to generate random numbers

--- src/TaskThreadDemo.java
+++ src/TaskThreadDemo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Nov 8, 2021
  * Lydia
@@ -18,19 +20,38 @@ public class TaskThreadDemo {
 		Runnable printA = new PrintChar('w', 100);
 		Runnable printB = new PrintChar('$', 200);
 		Runnable print1 = new PrintNum(400);
+		Runnable printRand = new Mystery();
 		
 		//Create threads
 		Thread threadA = new Thread(printA);
 		Thread threadB = new Thread(printB);
 		Thread threadC = new Thread(print1);
+		Thread threadD = new Thread(printRand);
 		
 		//start them up
+		threadD.start();
 		threadA.start();
 		threadB.start();
 		threadC.start();
 
 	}
 
+}
+
+class Mystery implements Runnable{
+	
+	private static Random rnd = new Random();
+
+	@Override
+	public void run() {
+		//print random numbers until the value 0 is generated
+		int v = rnd.nextInt(100);
+		while(v != 0) {
+			System.out.println(v);
+			v = rnd.nextInt(100);
+		}
+	}
+	
 }
 
 class PrintChar implements Runnable{
